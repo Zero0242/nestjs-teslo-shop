045c5cfe9044305ff8b315d20269c967aa08601a
Pedro Correa <pedrocorream66@gmail.com>
2023-08-07 03:26:02 -0400

    Servir la imagen subida con secureUrl

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,8 +1,11 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   ParseFilePipe,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
@@ -11,10 +14,15 @@ import { FileInterceptor } from '@nestjs/platform-express';
 
 import { diskStorage } from 'multer';
 import { fileFilter, fileNamer } from './helpers';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post('product')
   @UseInterceptors(
@@ -31,10 +39,25 @@ export class FilesController {
     @UploadedFile(ParseFilePipe)
     file: Express.Multer.File,
   ) {
-    if (!file) throw new BadRequestException('Make sure that file is an image');
+    if (!file) {
+      throw new BadRequestException('Make sure that file is an image');
+    }
+
+    const secureUrl =
+      this.configService.get('HOST_API') + '/files/product/' + file.filename;
 
     return {
-      filename: file.originalname,
+      secureUrl: secureUrl,
     };
+  }
+
+  @Get('product/:imageName')
+  findOneImage(@Res() res: Response, @Param('imageName') image: string) {
+    const path = this.filesService.getStaticProductImage(image);
+    /* res.status(403).json({
+      ok: false,
+      path: path,
+    }); */
+    res.sendFile(path);
   }
 }
