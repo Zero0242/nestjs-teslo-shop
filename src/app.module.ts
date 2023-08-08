14c2c5e3372f248083471d51a943ac395f8d7fc4
Pedro Correa <pedrocorream66@gmail.com>
2023-08-07 22:17:54 -0400

    Mostrar imagenes y correcciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,15 @@ import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
--- src/products/entities/product-image.entity.ts
+++ src/products/entities/product-image.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Product } from './product.entity';
 
-@Entity({ name: 'products' })
+@Entity({ name: 'images' })
 export class ProductImage {
   @PrimaryGeneratedColumn('uuid')
   id: string;
