17cb03ce3d16a2a368ad066ac6ec4b4325b06a6e
Pedro Correa <pedrocorream66@gmail.com>
2023-08-10 00:19:24 -0400

    Auth decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,11 +9,10 @@ import {
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
-import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
-import { RawHeader } from 'src/common/decorators/raw-headers.decorator';
 import { UserRoleGuard } from './guards/user-role.guard';
-import { RoleProtected } from './decorators/role-protected.decorator';
+import { RawHeader } from 'src/common/decorators/raw-headers.decorator';
+import { GetUser, Auth, RoleProtected } from './decorators';
 import { ValidRoles } from './interfaces';
 
 @Controller('auth')
@@ -52,6 +51,12 @@ export class AuthController {
   @RoleProtected(ValidRoles.superUser, ValidRoles.admin)
   @UseGuards(AuthGuard(), UserRoleGuard)
   privateRoute2(@GetUser() user: User) {
+    return { ok: true, user };
+  }
+
+  @Get('private3')
+  @Auth(ValidRoles.superUser, ValidRoles.admin)
+  privateRoute3(@GetUser() user: User) {
     return { ok: true, user };
   }
 }
