bc6efb9e1977b2b8d1df0e4d1f27b785153d11c6
Zero0242 <91895271+Zero0242@users.noreply.github.com>
2024-03-11 00:52:24 -0300

    JWT P1: configuracion imports de AUTHMODULE

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: config.get<string | number>('JWT_DURATION'),
+        },
+      }),
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
