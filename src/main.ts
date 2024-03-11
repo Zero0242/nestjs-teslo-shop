6763b1e39def16dd109205ca31765c45d7f0b21c
Zero0242 <91895271+Zero0242@users.noreply.github.com>
2024-03-10 23:45:09 -0300

    DATABASE: configuracion conexion posgres y modulo de config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+
+  app.enableCors();
+
+  const PORT: number = +process.env.PORT;
+  await app.listen(PORT, () => new Logger().log(`RUNNING ON PORT: ${PORT}`));
 }
 bootstrap();
