397df4064001fe819c34d499d653a38d56f90dc7
Zero0242 <91895271+Zero0242@users.noreply.github.com>
2024-03-11 00:24:22 -0300

    USERS: configuracion del servicio

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeepPartial, Repository } from 'typeorm';
+import { User } from 'src/entities';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  createUser(userDTO: DeepPartial<User>) {
+    return this.userRepository.create(userDTO);
+  }
+
+  saveUser(user: User) {
+    return this.userRepository.save(user);
+  }
+
+  findById(id: string) {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  findByEmail(email: string) {
+    return this.userRepository.findOneBy({ email });
+  }
+}
