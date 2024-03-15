b09bbcf6ea1790f910b0b6d5e5a307bd531734b0
Zero0242 <91895271+Zero0242@users.noreply.github.com>
2024-03-15 01:33:49 -0300

    INSERT PRODUCT: control de errores e interceptor de insert

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from 'src/entities';
@@ -6,12 +11,21 @@ import { CreateProductDto, UpdateProductDto } from './dto';
 
 @Injectable()
 export class ProductsService {
+  private readonly logger = new Logger('Product Service');
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
   ) {}
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
+
+      await this.productRepository.save(product);
+
+      return product;
+    } catch (error) {
+      this.errorHandler(error);
+    }
   }
 
   findAll() {
@@ -28,5 +42,12 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private errorHandler(error: any) {
+    this.logger.error(error);
+    if (error.code === '23505')
+      throw new BadRequestException('Producto ya existe');
+    throw new InternalServerErrorException('Llama al admin');
   }
 }
