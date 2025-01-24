269057c20c0fe3dda91c1cc66fabb963daa5d70a
Zero0242 <pedrocorream66@gmail.com>
2025-01-24 18:54:52 -0300

    Update multer-upload.decorator.ts

--- src/common/decorators/multer-upload.decorator.ts
+++ src/common/decorators/multer-upload.decorator.ts
@@ -1,6 +1,6 @@
 import { applyDecorators, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
+import { diskStorage, memoryStorage } from 'multer';
 import { FileType, MulterUtils } from '../helpers';
 
 interface Options {
@@ -22,6 +22,19 @@ export const MulterUpload = (
           destination: destination,
           filename: MulterUtils.uuidName,
         }),
+      }),
+    ),
+  );
+};
+
+export const MulterMemoryUpload = (fieldname: string, fileType?: FileType) => {
+  return applyDecorators(
+    UseInterceptors(
+      FileInterceptor(fieldname, {
+        fileFilter: MulterUtils.filter(fileType),
+        // * 10 mb
+        limits: { fileSize: 10 * 1024 * 1024 },
+        storage: memoryStorage(),
       }),
     ),
   );
